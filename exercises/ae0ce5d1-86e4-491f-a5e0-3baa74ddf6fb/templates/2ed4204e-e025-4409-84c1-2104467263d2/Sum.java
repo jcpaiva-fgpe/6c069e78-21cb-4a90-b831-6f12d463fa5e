c2e573adb5314464992e6c8d5467975855470a79
José Carlos <jose.c.paiva@inesctec.pt>
2020-04-29 11:16:55 +0100

    created file exercises/ae0ce5d1-86e4-491f-a5e0-3baa74ddf6fb/templates/2ed4204e-e025-4409-84c1-2104467263d2/Sum.java

---
+++ exercises/ae0ce5d1-86e4-491f-a5e0-3baa74ddf6fb/templates/2ed4204e-e025-4409-84c1-2104467263d2/Sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Sum {
+  private static final Scanner IN = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    int n1 = IN.nextInt(), n2 = IN.nextInt();
+    System.out.println(sum(n1, n2));
+  }
+
+  ${SOLUTION}
+}
